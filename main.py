e36e86ece9788ba0dcf598973029054a02ed78ef
Chipik123 <128310850+Chipik123@users.noreply.github.com>
2023-03-28 20:06:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-import pygame 
+from pygame import *
 
-window = display.set_mode((600,500))
+window = display.set_mode((600,500 ))
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
-        self.speed = player_speed
+        self.speed =player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
 
-def reset(self):
-    window.blit(self.image, (self.rect.x, self.rect.y))
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update_up(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.y > 5:
+        if keys[K_UP] == True:
+            self.rect.y = self.rect.y - self.speed
+           
+        if keys[K_DOWN] == True:
+            self.rect.y = self.rect.y + self.speed
 
-    def update_down(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_s] and self.rect.y < 450:
+        if keys[K_w] == True:
+            self.rect.y = self.rect.y - self.speed
+        if keys[K_s] == True:
+            self.rect.y = self.rect.y + self.speed
+window.fill((200,255,255))
+
+game = True
+finish = False
+
+clock = time.Clock()
+FPS = 60
